5ffb0aef54210b20f6bd6667579c3b534f1ca20e
irminagorecka <irmina.gorecka@gmail.com>
2022-06-15 00:30:12 +0200

    15.06 lab2b do setterów z pkt 6

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloApplication.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloApplication.java
@@ -1,9 +1,12 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -11,11 +14,24 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gusInfoGraphic.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root=fxmlLoader.load();
+        ModuleLayer.Controller controller=fxmlLoader.getController();
+//controller.setHostServices(this.getHostServices());
+//controller.setStage(primaryStage);
+
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,14 +1,101 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+
+import javafx.application.HostServices;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 
 public class HelloController {
+    public Label lbFile;
+    public ListView lstInfografiki;//referencja powiązana z kontrolką listy w GUI
+    public ImageView imgMiniaturka;
+    public TextField txtAdresStrony;
+    public Button btnPokazInfografike;
+    public Button btnPrzejdzDoStrony;
+    private Infografika selInfografika;
+
+
+
+    ObservableList<String> tytuly= FXCollections.observableArrayList(); //deklaracja i utworzenie pustej listy typu ObservableList<String>, FXCollections.observableArrayList() implementuje interfejs ObservableList<String>
+
+    GusInfoGraphicList igList;
+
+    FileChooser fileChooser=new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter=new FileChooser.ExtensionFilter("Pliki XML(*.xml)","*.xml"); //definicja filtra dla okna wyboru
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize(){ //kod inicjalizacji kontrolera
+        fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(//podpina procedurę obsługi zdarzenia do instancji selectedIndexProperty instancji SelectionModel (zwracanej przez getSelectionModel). Czyli: funkcjonalność zaznaczania jest realizowana w ListView przez instancję SelectionModel.
+                new ChangeListener<Number>() {// realizacja interfejsu ChangeListener<> czyli powiadamiania o zmianach
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                        int index=new_val.intValue();//konwersja na prymitywny typ int
+                        if (index!=-1){//sprawdza, czy index nie jest równa -1, sytuacja taka oznacza, że nic nie jest podświetlone na liście, i w linia adresu powinna być skasowana (linia 46)
+                        selInfografika=igList.infografiki.get(index);
+                        txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);//wprowadzenie do liniiadresu atrybutu infografiki, adresStrony
+                        Image image=new Image(igList.infografiki.get(index).adresMiniaturki);//utworzenie instancji Image na podstawie adresu URL grafiki z atrybutuadresMiniaturki
+                        imgMiniaturka.setImage(image);//ustawienie instancji z linii powyżej
+                        }
+                        else{
+                        txtAdresStrony.setText("");//kasowanie linii adresu
+                        imgMiniaturka.setImage(null);//wyczyszczenie zawartości
+                        selInfografika=null;
+                        }
+                    }
+                }
+
+
+        );
     }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {//okienko
+        File file=fileChooser.showOpenDialog(null);
+        if (file != null) {
+            igList=new GusInfoGraphicList(file.getAbsolutePath()); //utworzenie instancji klasy GusInfoGraphicList na pliku wskazanym w okienku wyboru
+            lbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki) tytuly.add(ig.tytul);//wypełnia listę obserwowalną tytuly wartościami atrybutów tytul obiektów Infografika znalezionymi przez GusInfoGraphicList w podanym pliku
+            lstInfografiki.setItems(tytuly);//podłącza listę tytuly do kontrolki ListView
+        }
+        else{
+            lbFile.setText("Proszę wczytać plik...");
+        }
+    }
+
+    private Stage stage;
+    private HostServices hostServices;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    public void setHostServices(HostServices hostServices) {
+        this.hostServices = hostServices;
+    }
+
+
+    public void btnPrzejdzDoStrony(ActionEvent actionEvent){
+        if (selInfografika!=null)
+            hostServices.showDocument(selInfografika.adresStrony);
+
+    }
+
+
+
+
 }
